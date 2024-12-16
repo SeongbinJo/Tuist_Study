55e572aaae723fd9ee5d2ecabe7e413855618851
avan31017 <avan31017@gmail.com>
2024-12-16 22:16:43 +0900

    swiftui 테스트

--- Tuist_Study/Sources/ContentView.swift
+++ Tuist_Study/Sources/ContentView.swift
@@ -5,11 +5,15 @@ public struct ContentView: View {
     public init() {}
 
     public var body: some View {
-        
         VStack {
             Text("hi")
             Text("Hello, World!")
                 .padding()
+            HStack {
+                Text("hstack")
+                Text("hstack 2")
+                Text("hstack 3")
+            }
         }
     }
 }
